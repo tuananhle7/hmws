0329478869be3654f57c0209f7f8836da65e1b21
Tuan Anh Le <tuananh@mit.edu>
2020-11-02 12:55:06 -0500

    render_heart

--- 2d_shapes/render.py
+++ 2d_shapes/render.py
@@ -1,6 +1,56 @@
 import torch
 
 
+def heart_occupancy_function(x, y):
+    """
+    Args
+        x [*shape]
+        y [*shape]
+
+    Returns bool tensor [*shape]
+    """
+    # Rescale so that it is inside [-1, 1]
+    x = x / 0.5
+    y = y / 0.5
+
+    return (x ** 2 + y ** 2 - 1) ** 3 - x ** 2 * y ** 3 < 0
+
+
+def render_heart(position_scale, canvas):
+    """Draws a heart on a canvas whose xy limits are [-1, 1].
+
+    Args
+        position_scale
+            position [2]
+            scale [] (scalar)
+        canvas [num_rows, num_cols]
+
+    Returns
+        new_canvas [num_rows, num_cols]
+    """
+    # Extract
+    position, scale = position_scale
+    position_x, position_y = position
+    num_rows, num_cols = canvas.shape
+    device = canvas.device
+
+    # Create xy points on the canvas
+    x_range = torch.linspace(-1, 1, steps=num_cols, device=device)
+    y_range = torch.linspace(-1, 1, steps=num_rows, device=device).flip(dims=[0])
+    # [num_cols, num_rows]
+    canvas_x, canvas_y = torch.meshgrid(x_range, y_range)
+    # [num_rows, num_cols]
+    canvas_x, canvas_y = canvas_x.T, canvas_y.T
+
+    # Draw heart
+    new_canvas = canvas.clone()
+    new_canvas[
+        heart_occupancy_function((canvas_x - position_x) / scale, (canvas_y - position_x) / scale)
+    ] = 1.0
+
+    return new_canvas
+
+
 def render_rectangle(xy_lims, canvas):
     """Draws a rectangle on a canvas whose xy limits are [-1, 1].
 
@@ -38,10 +88,15 @@ def render_rectangle(xy_lims, canvas):
 if __name__ == "__main__":
     num_rows, num_cols, device = 64, 64, "cuda"
     canvas = torch.zeros((num_rows, num_cols), device=device)
-    # tlwh = torch.tensor([-0.9, 0.9, 1, 0.1], device=device)
-    xy_lims = torch.tensor([-0.9, 0.9, 1, 0.1], device=device)
-    canvas = render_rectangle(xy_lims, canvas)
 
+    # rectangle
+    xy_lims = torch.tensor([-0.9, 0.9, 1, 0.1], device=device)
+    # canvas = render_rectangle(xy_lims, canvas)
+
+    # heart
+    position = torch.tensor([-0.5, -0.5], device=device)
+    scale = torch.tensor(0.5, device=device)
+    canvas = render_heart((position, scale), canvas)
     import matplotlib.pyplot as plt
     import util
 
