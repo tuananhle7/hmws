1a5cb0245fd1b737df87913735084daab5772c51
Tuan Anh Le <tuananh@mit.edu>
2021-02-11 21:03:43 -0500

    wip stacking model

---
+++ stacking/util.py
@@ -0,0 +1,28 @@
+import imageio
+import logging
+from pathlib import Path
+import sys
+import matplotlib.pyplot as plt
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s | %(pathname)s:%(lineno)d | %(levelname)s: %(message)s",
+    datefmt="%H:%M:%S",
+    stream=sys.stdout,
+)
+
+
+def save_fig(fig, path, dpi=100, tight_layout_kwargs={}):
+    Path(path).parent.mkdir(parents=True, exist_ok=True)
+    fig.tight_layout(**tight_layout_kwargs)
+    fig.savefig(path, bbox_inches="tight", dpi=dpi)
+    logging.info(f"Saved to {path}")
+    plt.close(fig)
+
+
+def make_gif(img_paths, gif_path, fps):
+    images = []
+    for img_path in img_paths:
+        images.append(imageio.imread(img_path))
+    imageio.mimsave(gif_path, images, duration=1 / fps)
+    logging.info(f"Saved to {gif_path}")
