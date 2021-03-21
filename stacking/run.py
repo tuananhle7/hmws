ec8a43ea303543ae0cb718e216530792760fa660
Tuan Anh Le <tuananh@mit.edu>
2021-03-21 11:02:30 -0400

    more test particles

--- stacking/run.py
+++ stacking/run.py
@@ -32,7 +32,7 @@ def get_args_parser():
     parser.add_argument("--seed", default=1, type=int, help=" ")
     parser.add_argument("--batch-size", default=5, type=int, help=" ")
     parser.add_argument("--num-particles", default=50, type=int, help=" ")
-    parser.add_argument("--test-num-particles", default=700, type=int, help=" ")
+    parser.add_argument("--test-num-particles", default=500, type=int, help=" ")
 
     # Model
     parser.add_argument("--num-primitives", default=3, type=int, help=" ")
