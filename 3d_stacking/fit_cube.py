bba2400e00167fc93cd2e7ab66423b4396716423
Tuan Anh Le <tuananh@mit.edu>
2021-03-29 11:33:20 -0400

    extract functions to render.py

--- 3d_stacking/fit_cube.py
+++ 3d_stacking/fit_cube.py
@@ -2,134 +2,7 @@ import pyredner
 import torch
 import util
 import matplotlib.pyplot as plt
-
-
-# Adapted from https://github.com/mikedh/trimesh/blob/master/trimesh/creation.py#L566
-def create_box(position, size):
-    # Extract
-    device = position.device
-
-    # vertices of the cube
-    centered_vertices = (
-        torch.tensor(
-            [0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1],
-            dtype=torch.float,
-            device=device,
-        ).view(-1, 3)
-        - 0.5
-    )
-    translation = position.clone()
-    translation[-1] += size / 2
-    vertices = centered_vertices * size + translation[None]
-
-    # hardcoded face indices
-    faces = torch.tensor(
-        [
-            1,
-            3,
-            0,
-            4,
-            1,
-            0,
-            0,
-            3,
-            2,
-            2,
-            4,
-            0,
-            1,
-            7,
-            3,
-            5,
-            1,
-            4,
-            5,
-            7,
-            1,
-            3,
-            7,
-            2,
-            6,
-            4,
-            2,
-            2,
-            7,
-            6,
-            6,
-            5,
-            4,
-            7,
-            5,
-            6,
-        ],
-        dtype=torch.int32,
-        device=device,
-    ).view(-1, 3)
-
-    return vertices, faces
-
-
-def render_cube(render_seed, size, color, position):
-    device = size.device
-
-    # Camera
-    cam = pyredner.Camera(
-        position=torch.tensor([0.5, 0.0, 0.4]),
-        look_at=torch.tensor([0.5, 1.0, 0.2]),
-        up=torch.tensor([0.0, 1.0, 0.0]),
-        fov=torch.tensor([45.0]),  # in degree
-        clip_near=1e-2,  # needs to > 0
-        resolution=(256, 256),
-        fisheye=False,
-    )
-
-    # Material
-    object_material = pyredner.Material(diffuse_reflectance=color)
-    white_material = pyredner.Material(
-        diffuse_reflectance=torch.tensor([1.0, 1.0, 1.0], device=device)
-    )
-    materials = [white_material, object_material]
-
-    # Shape
-    box_vertices, box_faces = create_box(position, size)
-    shape_object = pyredner.Shape(
-        vertices=box_vertices, indices=box_faces, uvs=None, normals=None, material_id=1
-    )
-    shape_light_top = pyredner.Shape(
-        vertices=torch.tensor(
-            [[0.0, 0.0, 1.0], [1.0, 0.0, 1.0], [0.0, 1.0, 1.0], [1.0, 1.0, 1.0]], device=device
-        ),
-        indices=torch.tensor([[0, 2, 1], [1, 2, 3]], dtype=torch.int32, device=device),
-        uvs=None,
-        normals=None,
-        material_id=0,
-    )
-    shape_light_front = pyredner.Shape(
-        vertices=torch.tensor(
-            [[0.0, 0.0, 0.0], [1.0, 0.0, 0.0], [0.0, 0.0, 1.0], [1.0, 0.0, 1.0]], device=device
-        ),
-        indices=torch.tensor([[0, 2, 1], [1, 2, 3]], dtype=torch.int32, device=device),
-        uvs=None,
-        normals=None,
-        material_id=0,
-    )
-
-    shapes = [shape_object, shape_light_top, shape_light_front]
-
-    # Light
-    light_top = pyredner.AreaLight(shape_id=1, intensity=torch.ones(3) * 2)
-    light_front = pyredner.AreaLight(shape_id=2, intensity=torch.ones(3) * 2)
-    area_lights = [light_top, light_front]
-
-    # Scene
-    scene = pyredner.Scene(cam, shapes, materials, area_lights)
-    scene_args = pyredner.RenderFunction.serialize_scene(scene=scene, num_samples=32, max_bounces=1)
-
-    # Render
-    render = pyredner.RenderFunction.apply
-    img = render(render_seed, *scene_args)
-
-    return img
+import render
 
 
 def plot(target_img, img, losses, iteration, path):
@@ -147,22 +20,23 @@ def plot(target_img, img, losses, iteration, path):
     util.save_fig(fig, path)
 
 
-def get_position(raw_x_position, true_position):
-    position = true_position.clone().detach()
-    position[0] = raw_x_position.sigmoid() * 0.8 + 0.1
-    return position
+def get_position(raw_position, true_position):
+    if raw_position.ndim == 0:
+        position = true_position.clone().detach()
+        position[0] = raw_position.sigmoid() * 0.8 + 0.1
+        return position
+    else:
+        pass
 
 
 if __name__ == "__main__":
-    # pyredner.set_use_gpu(False)
-    # device = "cpu"
     pyredner.set_use_gpu(torch.cuda.is_available())
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
     true_size = torch.tensor(0.1, device=device)
     true_position = torch.tensor([0.5, 0.8, 0.0], device=device)
     true_color = torch.tensor([1.0, 0, 0], device=device)
-    target_img = render_cube(0, true_size, true_color, true_position)
+    target_img = render.render_cube(true_size, true_color, true_position)
 
     raw_size = torch.tensor(-1.0, device=device, requires_grad=True)
     raw_color = torch.randn(3, device=device, requires_grad=True)
@@ -174,15 +48,10 @@ if __name__ == "__main__":
 
     for i in range(num_iterations):
         optimizer.zero_grad()
-        img = render_cube(
-            i + 1,
+        img = render.render_cube(
             raw_size.exp(),
             torch.softmax(raw_color, 0),
             get_position(raw_x_position, true_position)
-            # i + 1,
-            # raw_size.exp(),
-            # torch.softmax(raw_color, 0),
-            # true_position,
         )
         loss = (img - target_img).pow(2).sum()
         loss.backward()
---
+++ 3d_stacking/render.py
@@ -0,0 +1,154 @@
+import pyredner
+import torch
+import random
+
+
+def get_cube_mesh(position, size):
+    """Computes a cube mesh
+    Adapted from https://github.com/mikedh/trimesh/blob/master/trimesh/creation.py#L566
+
+    Args
+        position [3]
+        size []
+
+    Returns
+        vertices [num_vertices, 3]
+        faces [num_faces, 3]
+    """
+    # Extract
+    device = position.device
+
+    # vertices of the cube
+    centered_vertices = (
+        torch.tensor(
+            [0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1],
+            dtype=torch.float,
+            device=device,
+        ).view(-1, 3)
+        - 0.5
+    )
+    translation = position.clone()
+    translation[-1] += size / 2
+    vertices = centered_vertices * size + translation[None]
+
+    # hardcoded face indices
+    faces = torch.tensor(
+        [
+            1,
+            3,
+            0,
+            4,
+            1,
+            0,
+            0,
+            3,
+            2,
+            2,
+            4,
+            0,
+            1,
+            7,
+            3,
+            5,
+            1,
+            4,
+            5,
+            7,
+            1,
+            3,
+            7,
+            2,
+            6,
+            4,
+            2,
+            2,
+            7,
+            6,
+            6,
+            5,
+            4,
+            7,
+            5,
+            6,
+        ],
+        dtype=torch.int32,
+        device=device,
+    ).view(-1, 3)
+
+    return vertices, faces
+
+
+def render_cube(size, color, position, render_seed=None):
+    """Renders a cube given cube specs
+
+    Args
+        size []
+        color [3]
+        position [3]
+        render_seed (int) -- to be passed into the redner renderer
+
+    Returns rgb image [256, 256, 3]
+    """
+    # Extract
+    device = size.device
+    if render_seed is None:
+        render_seed = random.randrange(1000000)
+
+    # Camera
+    cam = pyredner.Camera(
+        position=torch.tensor([0.5, 0.0, 0.4]),
+        look_at=torch.tensor([0.5, 1.0, 0.2]),
+        up=torch.tensor([0.0, 1.0, 0.0]),
+        fov=torch.tensor([45.0]),  # in degree
+        clip_near=1e-2,  # needs to > 0
+        resolution=(256, 256),
+        fisheye=False,
+    )
+
+    # Material
+    object_material = pyredner.Material(diffuse_reflectance=color)
+    white_material = pyredner.Material(
+        diffuse_reflectance=torch.tensor([1.0, 1.0, 1.0], device=device)
+    )
+    materials = [white_material, object_material]
+
+    # Shape
+    box_vertices, box_faces = get_cube_mesh(position, size)
+    shape_object = pyredner.Shape(
+        vertices=box_vertices, indices=box_faces, uvs=None, normals=None, material_id=1
+    )
+    shape_light_top = pyredner.Shape(
+        vertices=torch.tensor(
+            [[0.0, 0.0, 1.0], [1.0, 0.0, 1.0], [0.0, 1.0, 1.0], [1.0, 1.0, 1.0]], device=device
+        ),
+        indices=torch.tensor([[0, 2, 1], [1, 2, 3]], dtype=torch.int32, device=device),
+        uvs=None,
+        normals=None,
+        material_id=0,
+    )
+    shape_light_front = pyredner.Shape(
+        vertices=torch.tensor(
+            [[0.0, 0.0, 0.0], [1.0, 0.0, 0.0], [0.0, 0.0, 1.0], [1.0, 0.0, 1.0]], device=device
+        ),
+        indices=torch.tensor([[0, 2, 1], [1, 2, 3]], dtype=torch.int32, device=device),
+        uvs=None,
+        normals=None,
+        material_id=0,
+    )
+
+    shapes = [shape_object, shape_light_top, shape_light_front]
+
+    # Light
+    light_top = pyredner.AreaLight(shape_id=1, intensity=torch.ones(3) * 2)
+    light_front = pyredner.AreaLight(shape_id=2, intensity=torch.ones(3) * 2)
+    area_lights = [light_top, light_front]
+
+    # Scene
+    scene = pyredner.Scene(cam, shapes, materials, area_lights)
+    scene_args = pyredner.RenderFunction.serialize_scene(scene=scene, num_samples=32, max_bounces=1)
+
+    # Render
+    render = pyredner.RenderFunction.apply
+    img = render(render_seed, *scene_args)
+
+    return img
