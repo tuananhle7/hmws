70979c04a589eef7b1808f6efa1cd2963a2d1c24
Tuan Anh Le <tuananh@mit.edu>
2021-04-08 12:33:16 -0400

    init

--- cmws/examples/__init__.py
+++ cmws/examples/__init__.py
@@ -1,4 +1,4 @@
 # Import common subpackages
-from cmws.examples.stacking import data, util
-from cmws.examples.stacking_3d import data, util
-from cmws.examples.csg import data, util
+from cmws.examples.stacking import data, util, run
+from cmws.examples.stacking_3d import data, util, run
+from cmws.examples.csg import data, util, run
