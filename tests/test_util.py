6058c844fe5237150e1607a32ff4639f03cfa64a
Tuan Anh Le <tuananh@mit.edu>
2021-05-03 16:43:22 -0400

    test for generalized condition_mvn

--- tests/test_util.py
+++ tests/test_util.py
@@ -4,24 +4,30 @@ import cmws.util
 
 def test_condition_mvn_shape():
     device = cmws.util.get_device()
-    batch_size = 2
     dim, dim_y = 10, 3
     dim_x = dim - dim_y
 
-    # Init p(x, y)
-    loc = torch.randn((batch_size, dim), device=device)
-    cov = torch.eye(dim, device=device)[None].expand(batch_size, dim, dim)
-    multivariate_normal_dist = torch.distributions.MultivariateNormal(loc, cov)
+    for shape in [[], [2], [2, 3]]:
+        num_elements = cmws.util.get_num_elements(shape)
 
-    # Init y
-    y = torch.randn((batch_size, dim_y), device=device)
+        # Init p(x, y)
+        loc = torch.randn(*[*shape, dim], device=device)
+        cov = (
+            torch.eye(dim, device=device)[None]
+            .expand(num_elements, dim, dim)
+            .view(*[*shape, dim, dim])
+        )
+        multivariate_normal_dist = torch.distributions.MultivariateNormal(loc, cov)
 
-    # Compute p(x | y)
-    conditioned_multivariate_normal_dist = cmws.util.condition_mvn(multivariate_normal_dist, y)
+        # Init y
+        y = torch.randn(*[*shape, dim_y], device=device)
 
-    # Extract params of p(x | y)
-    loc_new = conditioned_multivariate_normal_dist.mean
-    cov_new = conditioned_multivariate_normal_dist.covariance_matrix
+        # Compute p(x | y)
+        conditioned_multivariate_normal_dist = cmws.util.condition_mvn(multivariate_normal_dist, y)
 
-    assert list(loc_new.shape) == [batch_size, dim_x]
-    assert list(cov_new.shape) == [batch_size, dim_x, dim_x]
+        # Extract params of p(x | y)
+        loc_new = conditioned_multivariate_normal_dist.mean
+        cov_new = conditioned_multivariate_normal_dist.covariance_matrix
+
+        assert list(loc_new.shape) == shape + [dim_x]
+        assert list(cov_new.shape) == shape + [dim_x, dim_x]
