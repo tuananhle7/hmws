4d46e992f73a36b617d00be81707a98a7d94cc34
Tuan Anh Le <tuananh@mit.edu>
2021-05-05 20:44:46 -0400

    --sleep-pretraining-batch-size

--- cmws/train.py
+++ cmws/train.py
@@ -44,6 +44,13 @@ def train(model, optimizer, stats, args):
         util.logging.info(f"Using pyro version {pyro.__version__}")
 
     # Sleep pretraining
+    if "sleep_pretraining_batch_size" in args:
+        if args.sleep_pretraining_batch_size == 0:
+            sleep_pretraining_batch_size = args.num_particles * args.batch_size
+        else:
+            sleep_pretraining_batch_size = args.sleep_pretraining_batch_size
+    else:
+        sleep_pretraining_batch_size = args.num_particles * args.batch_size
     for iteration in range(
         num_sleep_pretraining_iterations_so_far, args.num_sleep_pretraining_iterations
     ):
@@ -55,7 +62,7 @@ def train(model, optimizer, stats, args):
 
             # Evaluate loss
             loss = losses.get_sleep_loss(
-                generative_model, guide, args.num_particles * args.batch_size
+                generative_model, guide, sleep_pretraining_batch_size
             ).mean()
 
             # Compute gradient
