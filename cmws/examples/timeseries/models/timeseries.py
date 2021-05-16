5f4ac77794ce5c335762a334f1f3bac10f4063d5
Luke Hewitt <insperatum@gmail.com>
2021-05-16 13:09:31 -0400

    Better initialization of continuous latents

--- cmws/examples/timeseries/models/timeseries.py
+++ cmws/examples/timeseries/models/timeseries.py
@@ -29,6 +29,8 @@ class GenerativeModel(nn.Module):
         self.gp_params_extractor = nn.Linear(
             self.lstm_hidden_dim, 2 * timeseries_util.gp_params_dim
         )
+        self.gp_params_extractor.weight.data.fill_(0)
+        self.gp_params_extractor.bias.data.fill_(0)
 
     @property
     def device(self):
@@ -465,7 +467,7 @@ class GenerativeModel(nn.Module):
             ).sample(sample_shape)
         except Exception as e:
             raise RuntimeError(f"MVN sample error: {e}")
-
+            
         return obs
 
     @torch.no_grad()
@@ -580,6 +582,8 @@ class Guide(nn.Module):
         self.gp_params_extractor = nn.Linear(
             self.lstm_hidden_dim, 2 * timeseries_util.gp_params_dim
         )
+        self.gp_params_extractor.weight.data.fill_(0)
+        self.gp_params_extractor.bias.data.fill_(0)
 
     @property
     def device(self):
