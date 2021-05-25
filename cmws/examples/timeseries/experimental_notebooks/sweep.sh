cf4de6b98b30a7b0830b22c42ee84e4c99128378
Luke Hewitt <insperatum@gmail.com>
2021-05-25 14:33:55 -0400

    expt5

--- cmws/examples/timeseries/experimental_notebooks/sweep.sh
+++ cmws/examples/timeseries/experimental_notebooks/sweep.sh
@@ -1,20 +1,20 @@
 sbatch_args="--partition=tenenbaum --time=9:00:00 --mem=8G -c2"
 sbatch_plot_args="--partition=tenenbaum --time=9:00:00 --mem=16G -c2"
 
-for num_particles in 15; do
-    for memory_size in 10; do
-        for num_proposals_mws in 10; do
+for num_particles in 5 15; do
+    for memory_size in 3 10; do
+        for num_proposals_mws in 5 15; do
            lr_guide_continuous=0.01
            lr_guide_discrete=0.01
-	   for lr_prior_continuous in 0.0 0.01; do
+	   for lr_prior_continuous in 0.0; do
            #lr_prior_continuous=0.0
            lr_prior_discrete=0.0
-           lr_likelihood=0.01
-            for include_symbols in  "WRCP12345Ll" "WRCP12345L!@#$%" "WRCP12345LlXabcde" "WRCP12345L!@#$%Xabcde"; do
+           lr_likelihood=0.02
+            for include_symbols in "WRCP12345Ll" "WRCP12345L!@#$%"; do
                 for learn_eps in "--learn-eps" ""; do
-                experiment_name=expt5_particles${num_particles}_memory${memory_size}_proposals${num_proposals_mws}_symbols${include_symbols}$learn_eps
-                algorithm=cmws_4
-                cmd="sbatch $sbatch_args --output=logs/cmws_4_$experiment_name.out ./run.sh $experiment_name $algorithm
+                experiment_name=expt5_particles${num_particles}_memory${memory_size}_proposals${num_proposals_mws}_symbols${include_symbols}_lrc=${lr_prior_continuous}$learn_eps
+                algorithm=cmws_5
+                cmd="sbatch $sbatch_args --output=logs/${algorithm}_$experiment_name.out ./run.sh $experiment_name $algorithm
                     --continue-training
                     --num-particles=$num_particles
                     --full-training-data
@@ -38,7 +38,7 @@ for num_particles in 15; do
 
                 total_num_particles=`expr $num_particles \* \( $memory_size + $num_proposals_mws \)`
                 algorithm=rws
-                cmd="sbatch $sbatch_args --output=logs/rws_$experiment_name.out ./run.sh $experiment_name $algorithm
+                cmd="sbatch $sbatch_args --output=logs/${algorithm}_$experiment_name.out ./run.sh $experiment_name $algorithm
                     --continue-training
                     --num-particles=$total_num_particles
                     --full-training-data
