d6154c1826795d8a671355e1b3d32af50c9a2fec
Luke Hewitt <insperatum@gmail.com>
2021-05-25 16:40:51 -0400

    expt6

--- cmws/examples/timeseries/experimental_notebooks/sweep.sh
+++ cmws/examples/timeseries/experimental_notebooks/sweep.sh
@@ -1,20 +1,21 @@
-sbatch_args="--partition=tenenbaum --time=9:00:00 --mem=8G -c2"
-sbatch_plot_args="--partition=tenenbaum --time=9:00:00 --mem=16G -c2"
+sbatch_args="--partition=tenenbaum --time=5:00:00 --mem=8G -c2"
+sbatch_plot_args="--partition=tenenbaum --time=5:00:00 --mem=16G -c2"
 
-for num_particles in 5 15; do
-    for memory_size in 3 10; do
-        for num_proposals_mws in 5 15; do
+for num_particles in 5; do
+    for memory_size in 3; do
+        for num_proposals_mws in 10; do
            lr_guide_continuous=0.01
            lr_guide_discrete=0.01
-	   for lr_prior_continuous in 0.0; do
+	   for lr_prior_continuous in 0.0 0.01; do
            #lr_prior_continuous=0.0
            lr_prior_discrete=0.0
            lr_likelihood=0.02
-            for include_symbols in "WRCP12345Ll" "WRCP12345L!@#$%"; do
+            for include_symbols in "WRCP12345L!@#$%" "WRCP12345L!@#$%Xabcde"; do
                 for learn_eps in "--learn-eps" ""; do
-			experiment_name=expt5_particles${num_particles}_memory${memory_size}_proposals${num_proposals_mws}_symbols${include_symbols}_lrc=${lr_prior_continuous}$learn_eps
+		   experiment_name=expt6_K${num_particles}_M${memory_size}_N${num_proposals_mws}_symbols${include_symbols}_lrc=${lr_prior_continuous}$learn_eps
+		   for seed in 1 2 3; do
 			algorithm=cmws_5
-			cmd="sbatch $sbatch_args --output=logs/${algorithm}_$experiment_name.out ./run.sh $experiment_name $algorithm
+			cmd="sbatch $sbatch_args --output=logs/${algorithm}_${experiment_name}_seed${seed}.out ./run.sh $experiment_name $algorithm
 			    --continue-training
 			    --num-particles=$num_particles
 			    --full-training-data
@@ -32,13 +33,14 @@ for num_particles in 5 15; do
 			    --lr-sleep-pretraining=0.01
 			    --num-sleep-pretraining-iterations=1000
 			    --include-symbols=$include_symbols
+                            --seed=$seed
 			    $learn_eps"
 			echo $cmd
 			eval $cmd
 
 			total_num_particles=`expr $num_particles \* \( $memory_size + $num_proposals_mws \)`
 			algorithm=rws
-			cmd="sbatch $sbatch_args --output=logs/${algorithm}_$experiment_name.out ./run.sh $experiment_name $algorithm
+			cmd="sbatch $sbatch_args --output=logs/${algorithm}_${experiment_name}_seed${seed}.out ./run.sh $experiment_name $algorithm
 			    --continue-training
 			    --num-particles=$total_num_particles
 			    --full-training-data
@@ -54,13 +56,16 @@ for num_particles in 5 15; do
 			    --lr-sleep-pretraining=0.01
 			    --num-sleep-pretraining-iterations=1000
 			    --include-symbols=$include_symbols
+			    --seed=$seed
 			    $learn_eps"
 			echo $cmd
 			eval $cmd
 
-			cmd="sbatch $sbatch_plot_args --output=logs/plot_$experiment_name.out ./plot.sh $experiment_name"	
-			echo $cmd
-			eval $cmd
+                   done
+
+	           cmd="sbatch $sbatch_plot_args --output=logs/plot_$experiment_name.out ./plot.sh $experiment_name"	
+		   echo $cmd
+		   eval $cmd
 		done
             done
           done
