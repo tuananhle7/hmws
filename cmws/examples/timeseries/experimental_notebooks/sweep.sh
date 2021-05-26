ad1e7e54e5ccd5d58fadd518e1e097d7b7e905c7
Luke Hewitt <insperatum@gmail.com>
2021-05-26 12:23:07 -0400

    expt8

--- cmws/examples/timeseries/experimental_notebooks/sweep.sh
+++ cmws/examples/timeseries/experimental_notebooks/sweep.sh
@@ -1,19 +1,19 @@
 sbatch_args="--partition=tenenbaum --time=5:00:00 --mem=8G -c2"
 sbatch_plot_args="--partition=tenenbaum --time=5:00:00 --mem=16G -c2"
 
-for num_particles in 5; do
+for num_particles in 3; do
     for memory_size in 3; do
-        for num_proposals_mws in 10; do
+        for num_proposals_mws in 3 10; do
            lr_guide_continuous=0.01
            lr_guide_discrete=0.01
 	   for lr_prior_continuous in 0.0; do
            #lr_prior_continuous=0.0
            lr_prior_discrete=0.0
            lr_likelihood=0.01
-            for include_symbols in "WRCP12345L!@#$%"; do
-                for learn_eps in "--learn-eps" ""; do
-		   experiment_name=expt7_K${num_particles}_M${memory_size}_N${num_proposals_mws}_symbols${include_symbols}_lrc=${lr_prior_continuous}$learn_eps
-		   for seed in 1 2 3 4 5 6; do
+            for include_symbols in "WRCP12" "WRCP12345" "WRCP1234567890"; do
+                for learn_eps in "--learn-eps"; do
+		   experiment_name=expt8_K${num_particles}_M${memory_size}_N${num_proposals_mws}_symbols${include_symbols}_lrc=${lr_prior_continuous}$learn_eps
+		   for seed in 1 2; do
 			algorithm=cmws_5
 			cmd="sbatch $sbatch_args --output=logs/${algorithm}_${experiment_name}_seed${seed}.out ./run.sh $experiment_name $algorithm
 			    --continue-training
@@ -24,7 +24,6 @@ for num_particles in 5; do
 			    --memory-size=$memory_size
 			    --num-proposals-mws=$num_proposals_mws
 			    --max-num-chars=9
-			    --lr=0.0
 			    --lr-guide-continuous=$lr_guide_continuous
 			    --lr-guide-discrete=$lr_guide_discrete
 			    --lr-prior-continuous=$lr_prior_continuous
@@ -47,7 +46,6 @@ for num_particles in 5; do
 			    --generative-model-lstm-hidden-dim=10
 			    --guide-lstm-hidden-dim=10
 			    --max-num-chars=9
-			    --lr=0.0
 			    --lr-guide-continuous=$lr_guide_continuous
 			    --lr-guide-discrete=$lr_guide_discrete
 			    --lr-prior-continuous=$lr_prior_continuous
