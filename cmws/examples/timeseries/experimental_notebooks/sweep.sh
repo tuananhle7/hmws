cdf39b4b21388704ed19385f284da6f4213e2b1c
Luke Hewitt <insperatum@gmail.com>
2021-05-27 17:17:08 -0400

    expt17

--- cmws/examples/timeseries/experimental_notebooks/sweep.sh
+++ cmws/examples/timeseries/experimental_notebooks/sweep.sh
@@ -1,79 +1,86 @@
 sbatch_args="--partition=tenenbaum --time=3:00:00 --mem=8G -c2"
 sbatch_plot_args="--partition=tenenbaum --time=3:00:00 --mem=16G -c2"
 
-num_iterations=2000
+num_iterations=2500
 test_interval=100
+
+max_period=0.5
+
+for lr in 0.005 0.0005; do
 
 for max_num_chars in 9; do
 for hidden in 10; do
 for num_particles in 3 5; do # 8; do
 for memory_size in 2; do
 for num_proposals_mws in 8; do
-lr_guide_continuous=0.01
-for lr_guide_discrete in 0.01; do
-for lr_prior_continuous in 0.0 0.001; do #lr_prior_continuous=0.0
-lr_prior_discrete=$lr_prior_continuous
-lr_likelihood=0.01
+lr_guide_continuous=$lr
+for lr_guide_discrete in $lr; do
+for lr_prior_continuous in $lr; do #lr_prior_continuous=0.0
+lr_prior_discrete=0 #$lr_prior_continuous
+lr_likelihood=$lr
 for include_symbols in "WRCP1234567890Ll"; do
 for learn_eps in "--learn-eps"; do
 for allow_repeat_factors in ""; do #"--allow_repeat_factors" ""; do
 for learn_coarse in "--learn-coarse"; do
 for insomnia in 0.5; do
-    experiment_name=expt16_K${num_particles}_M${memory_size}_N${num_proposals_mws}_h${hidden}_c${max_num_chars}_symbols${include_symbols}_ins${insomnia}_lrpc=${lr_prior_continuous}_lrgd=${lr_guide_discrete}$learn_eps$allow_repeat_factors$learn_coarse
-    for seed in 1 2 3 4 5 6 7 8 9 10; do
-	#algorithm=cmws_5
-	#cmd="sbatch $sbatch_args --output=logs/${algorithm}_${experiment_name}_seed${seed}.out ./run.sh $experiment_name $algorithm $seed
-	#    --continue-training
-	#    --num-iterations=$num_iterations
-        #    --test-interval=$test_interval
-	#    --num-particles=$num_particles
-	#    --full-training-data
-	#    --generative-model-lstm-hidden-dim=$hidden
-	#    --guide-lstm-hidden-dim=$hidden
-	#    --memory-size=$memory_size
-	#    --num-proposals-mws=$num_proposals_mws
-	#    --max-num-chars=$max_num_chars
-	#    --lr-guide-continuous=$lr_guide_continuous
-	#    --lr-guide-discrete=$lr_guide_discrete
-	#    --lr-prior-continuous=$lr_prior_continuous
-	#    --lr-prior-discrete=$lr_prior_discrete
-	#    --lr-likelihood=$lr_likelihood
-	#    --lr-sleep-pretraining=0.01
-	#    --num-sleep-pretraining-iterations=1000
-	#    --include-symbols=$include_symbols
-        #    --insomnia=$insomnia
-	#    $learn_eps
-	#    $allow_repeat_factors
-        #    $learn_coarse
-	#    "
-	#echo $cmd
-	#eval $cmd
-	#total_num_particles=`expr $num_particles \* \( $memory_size + $num_proposals_mws \)`
-	#algorithm=rws
-	#cmd="sbatch $sbatch_args --output=logs/${algorithm}_${experiment_name}_seed${seed}.out ./run.sh $experiment_name $algorithm $seed
-	#    --continue-training
-	#    --num-iterations=$num_iterations
-        #    --test-interval=$test_interval
-	#    --num-particles=$total_num_particles
-	#    --full-training-data
-	#    --generative-model-lstm-hidden-dim=$hidden
-	#    --guide-lstm-hidden-dim=$hidden
-	#    --max-num-chars=$max_num_chars
-	#    --lr-guide-continuous=$lr_guide_continuous
-	#    --lr-guide-discrete=$lr_guide_discrete
-	#    --lr-prior-continuous=$lr_prior_continuous
-	#    --lr-prior-discrete=$lr_prior_discrete
-	#    --lr-likelihood=$lr_likelihood
-	#    --lr-sleep-pretraining=0.01
-	#    --num-sleep-pretraining-iterations=1000
-	#    --include-symbols=$include_symbols
-        #    --insomnia=$insomnia
-	#    $learn_eps
-	#    $allow_repeat_factors
-        #    $learn_coarse
-	#    "
-	#echo $cmd
-	#eval $cmd
+    experiment_name=expt17_K${num_particles}_M${memory_size}_N${num_proposals_mws}_h${hidden}_c${max_num_chars}_symbols${include_symbols}_ins${insomnia}_per${max_period}_lrpc=${lr_prior_continuous}_lrgd=${lr_guide_discrete}$learn_eps$allow_repeat_factors$learn_coarse
+    for seed in 1 2 3; do
+        echo seed=$seed
+	algorithm=cmws_5
+	cmd="sbatch $sbatch_args --output=logs/${algorithm}_${experiment_name}_seed${seed}.out ./run.sh $experiment_name $algorithm $seed
+	    --continue-training
+	    --num-iterations=$num_iterations
+            --test-interval=$test_interval
+	    --num-particles=$num_particles
+	    --full-training-data
+	    --generative-model-lstm-hidden-dim=$hidden
+	    --guide-lstm-hidden-dim=$hidden
+	    --memory-size=$memory_size
+	    --num-proposals-mws=$num_proposals_mws
+	    --max-num-chars=$max_num_chars
+	    --lr-guide-continuous=$lr_guide_continuous
+	    --lr-guide-discrete=$lr_guide_discrete
+	    --lr-prior-continuous=$lr_prior_continuous
+	    --lr-prior-discrete=$lr_prior_discrete
+	    --lr-likelihood=$lr_likelihood
+	    --lr-sleep-pretraining=0.01
+	    --num-sleep-pretraining-iterations=1000
+	    --include-symbols=$include_symbols
+            --insomnia=$insomnia
+            --max-period=$max_period
+	    $learn_eps
+	    $allow_repeat_factors
+            $learn_coarse
+	    "
+	echo $cmd
+	eval $cmd
+	total_num_particles=`expr $num_particles \* \( $memory_size + $num_proposals_mws \)`
+	algorithm=rws
+	cmd="sbatch $sbatch_args --output=logs/${algorithm}_${experiment_name}_seed${seed}.out ./run.sh $experiment_name $algorithm $seed
+	    --continue-training
+	    --num-iterations=$num_iterations
+            --test-interval=$test_interval
+	    --num-particles=$total_num_particles
+	    --full-training-data
+	    --generative-model-lstm-hidden-dim=$hidden
+	    --guide-lstm-hidden-dim=$hidden
+	    --max-num-chars=$max_num_chars
+	    --lr-guide-continuous=$lr_guide_continuous
+	    --lr-guide-discrete=$lr_guide_discrete
+	    --lr-prior-continuous=$lr_prior_continuous
+	    --lr-prior-discrete=$lr_prior_discrete
+	    --lr-likelihood=$lr_likelihood
+	    --lr-sleep-pretraining=0.01
+	    --num-sleep-pretraining-iterations=1000
+	    --include-symbols=$include_symbols
+            --insomnia=$insomnia
+            --max-period=$max_period
+	    $learn_eps
+	    $allow_repeat_factors
+            $learn_coarse
+	    "
+	echo $cmd
+	eval $cmd
 
 	total_num_particles=`expr $num_particles \* \( $memory_size + $num_proposals_mws \)`
 	algorithm=vimco_2
@@ -95,6 +102,7 @@ for insomnia in 0.5; do
 	    --num-sleep-pretraining-iterations=1000
 	    --include-symbols=$include_symbols
             --insomnia=$insomnia
+            --max-period=$max_period
 	    $learn_eps
 	    $allow_repeat_factors
             $learn_coarse
@@ -122,6 +130,7 @@ for insomnia in 0.5; do
 	    --num-sleep-pretraining-iterations=1000
 	    --include-symbols=$include_symbols
             --insomnia=$insomnia
+            --max-period=$max_period
 	    $learn_eps
 	    $allow_repeat_factors
             $learn_coarse
@@ -134,6 +143,7 @@ for insomnia in 0.5; do
     echo $cmd
     eval $cmd
 
+done
 done
 done
 done
