178d9966226079aa095b6fc7d305a8ed131ea4d7
Tuan Anh Le <tuananh@mit.edu>
2021-09-30 13:27:55 -0400

    starting run.py

---
+++ cmws/examples/switching_ssm/run.py
@@ -0,0 +1,97 @@
+from pathlib import Path
+
+from cmws import train, util
+from cmws.examples.switching_ssm import util as switching_ssm_util
+
+
+def main(args):
+    # Cuda
+    device = util.get_device()
+
+    # Seed
+    util.set_seed(args.seed)
+
+    # Initialize models, optimizer, stats, data
+    checkpoint_path = util.get_checkpoint_path(args.experiment_name, get_config_name(args))
+    if not (args.continue_training and Path(checkpoint_path).exists()):
+        util.logging.info("Training from scratch")
+        model, optimizer, stats = switching_ssm_util.init(args, device)
+    else:
+        model, optimizer, stats, _ = switching_ssm_util.load_checkpoint(checkpoint_path, device)
+
+    # Train
+    # train.train(model, optimizer, stats, args)
+
+
+def get_config_name(args):
+    return f"{args.algorithm}_{args.seed}"
+
+
+def get_args_parser():
+    import argparse
+
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--experiment-name", default="", help=" ")
+    parser.add_argument(
+        "--algorithm",
+        default="rws",
+        choices=[
+            "rws",
+            "elbo",
+            "vimco",
+            "vimco_2",
+            "cmws",
+            "cmws_2",
+            "cmws_3",
+            "cmws_4",
+            "cmws_5",
+            "reinforce",
+        ],
+        help=" ",
+    )
+    parser.add_argument("--seed", default=1, type=int, help=" ")
+    parser.add_argument("--batch-size", default=5, type=int, help=" ")
+    parser.add_argument("--num-particles", default=50, type=int, help=" ")
+    parser.add_argument("--memory-size", default=10, type=int, help=" ")
+    parser.add_argument("--num-proposals-mws", default=10, type=int, help=" ")
+    parser.add_argument("--test-num-particles", default=100, type=int, help=" ")
+
+    # Data
+
+    # Model
+    parser.add_argument("--num-states", default=5, type=int, help=" ")
+    parser.add_argument("--continuous-dim", default=2, type=int, help=" ")
+    parser.add_argument("--obs-dim", default=10, type=int, help=" ")
+    parser.add_argument("--num-timesteps", default=100, type=int, help=" ")
+    parser.add_argument(
+        "--model-type", default="slds", choices=["slds",], help=" ",
+    )
+
+    # Optimization
+    parser.add_argument("--continue-training", action="store_true", help=" ")
+    parser.add_argument("--num-iterations", default=1000, type=int, help=" ")
+    parser.add_argument("--num-sleep-pretraining-iterations", default=0, type=int, help=" ")
+    parser.add_argument("--sleep-pretraining-batch-size", default=0, type=int, help=" ")
+    parser.add_argument("--lr", default=1e-3, type=float, help=" ")
+    parser.add_argument(
+        "--insomnia",
+        default=1.0,
+        type=float,
+        help="only applicable for RWS for pyro models - 1.0 means Wake-Wake, 0.0 means Wake-Sleep,"
+        "otherwise it's inbetween",
+    )
+    parser.add_argument(
+        "--continuous-guide-lr", default=1e-3, type=float, help=" ",
+    )
+    parser.add_argument("--log-interval", default=1, type=int, help=" ")
+    parser.add_argument("--save-interval", default=10, type=int, help=" ")
+    parser.add_argument("--test-interval", default=100, type=int, help=" ")
+    parser.add_argument("--checkpoint-interval", default=1000, type=int, help=" ")
+
+    return parser
+
+
+if __name__ == "__main__":
+    parser = get_args_parser()
+    args = parser.parse_args()
+    main(args)
---
+++ cmws/examples/switching_ssm/util.py
@@ -0,0 +1,97 @@
+import collections
+import time
+import logging
+from pathlib import Path
+import torch
+import itertools
+import cmws
+from cmws.examples.switching_ssm.models import slds
+
+
+# Init, saving, etc
+def init(run_args, device):
+    memory = None
+    if run_args.model_type == "slds":
+        # Generative model
+        generative_model = slds.GenerativeModel(
+            num_states=run_args.num_states,
+            continuous_dim=run_args.continuous_dim,
+            obs_dim=run_args.obs_dim,
+            num_timesteps=run_args.num_timesteps,
+        ).to(device)
+
+        # Guide
+        guide = slds.Guide(
+            num_states=run_args.num_states,
+            continuous_dim=run_args.continuous_dim,
+            obs_dim=run_args.obs_dim,
+            num_timesteps=run_args.num_timesteps,
+        ).to(device)
+
+        # Memory
+        if "mws" in run_args.algorithm:
+            memory = cmws.memory.Memory(
+                1, run_args.memory_size, generative_model, check_unique=True,
+            ).to(device)
+
+    # Model dict
+    model = {"generative_model": generative_model, "guide": guide, "memory": memory}
+
+    # Optimizer
+    parameters = itertools.chain(generative_model.parameters(), guide.parameters())
+    optimizer = torch.optim.Adam(parameters, lr=run_args.lr)
+
+    # Stats
+    stats = Stats([], [], [], [])
+
+    return model, optimizer, stats
+
+
+def save_checkpoint(path, model, optimizer, stats, run_args=None):
+    Path(path).parent.mkdir(parents=True, exist_ok=True)
+    generative_model, guide, memory = model["generative_model"], model["guide"], model["memory"]
+    torch.save(
+        {
+            "generative_model_state_dict": generative_model.state_dict(),
+            "guide_state_dict": guide.state_dict(),
+            "memory_state_dict": None if memory is None else memory.state_dict(),
+            "optimizer_state_dict": optimizer.get_state()
+            if "_pyro" in run_args.model_type
+            else optimizer.state_dict(),
+            "stats": stats,
+            "run_args": run_args,
+        },
+        path,
+    )
+    logging.info(f"Saved checkpoint to {path}")
+
+
+def load_checkpoint(path, device, num_tries=3):
+    for i in range(num_tries):
+        try:
+            checkpoint = torch.load(path, map_location=device)
+            break
+        except Exception as e:
+            logging.info(f"Error {e}")
+            wait_time = 2 ** i
+            logging.info(f"Waiting for {wait_time} seconds")
+            time.sleep(wait_time)
+    run_args = checkpoint["run_args"]
+    model, optimizer, stats = init(run_args, device, fast=True)
+
+    generative_model, guide, memory = model["generative_model"], model["guide"], model["memory"]
+    guide.load_state_dict(checkpoint["guide_state_dict"])
+    generative_model.load_state_dict(checkpoint["generative_model_state_dict"])
+    if memory is not None:
+        memory.load_state_dict(checkpoint["memory_state_dict"])
+
+    model = {"generative_model": generative_model, "guide": guide, "memory": memory}
+    if "_pyro" in run_args.model_type:
+        optimizer.set_state(checkpoint["optimizer_state_dict"])
+    else:
+        optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
+    stats = checkpoint["stats"]
+    return model, optimizer, stats, run_args
+
+
+Stats = collections.namedtuple("Stats", ["losses", "sleep_pretraining_losses", "log_ps", "kls"])
