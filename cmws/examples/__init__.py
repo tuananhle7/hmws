6cd0e64e65c3ec39776cfc72c6060ea4fc311f9a
Tuan Anh Le <tuananh@mit.edu>
2021-10-02 21:52:04 -0400

    init

--- cmws/examples/__init__.py
+++ cmws/examples/__init__.py
@@ -4,3 +4,4 @@ from cmws.examples.stacking_3d import data, util, run
 from cmws.examples.csg import data, util, run
 from cmws.examples.timeseries_real import data, util, run
 from cmws.examples.scene_understanding import data, util, run
+from cmws.examples.switching_ssm import data, util, run
